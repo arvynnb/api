af847c44eb80ac7f8abfe7be5b6041d1a108e5cc
Arvynn Banez <arvynn.c.banez@gmail.com>
2019-08-15 00:10:46 +0800

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,4 +14,10 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::apiResource('/products','ProductController');
+
+Route::group(['prefix' => 'products'], function () {
+    Route::apiResource('/{product}/reviews','ReviewController');
 });
